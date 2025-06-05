d126e3ff4cdaba6ea3a3a4d621e28aee7c90a097
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-06-05 15:09:08 +0700

    Menunjukkan current data yang ada didalam list

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -63,6 +63,15 @@ public:
             return; //If the stack is empty, print a message and return
         }
         else
-
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data<< "  " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }// Return the value of the top node
     }
-}
+};
+
