55195f8d30318fde0c97a0211c84873621bb2a5c
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-06-05 14:53:34 +0700

    Membuat operasi untuk melakukan pengecekan status stack

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -35,4 +35,10 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    //IsEmpty operation: Check if the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL; // Return true if the top pointer is NULL, indicating an empty stack
+    }
 }
